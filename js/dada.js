4395353e27b5ac6fd08359c77b0f7565ea3aa234
DLorangg <lorang.damian.epet20@gmail.com>
2022-11-09 18:29:35 -0300

    Problemas con la rama

--- js/dada.js
+++ js/dada.js
@@ -20,19 +20,3 @@ function moverPag(){
 
     }
 }
-
-//movimiento del boton
-function validate(){
-     // check if input is bigger than 2
-    var value = document.getElementById('inp').value;
-    if (value.length < 2) {
-        alert("falso");
-        return false; // keep form from submitting
-    }
-
- // else form is good let it submit, of course you will 
- // probably want to alert the user WHAT went wrong.
-    alert("verdadero");
-    return true;
-}
-/*https://stackoverflow.com/questions/12164751/check-input-value-length*/
