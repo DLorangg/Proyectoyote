4abc9d19181e6691e52caab35a3e7a840727d05a
DLorangg <lorang.damian.epet20@gmail.com>
2022-11-14 16:58:20 -0300

    Cambios

--- js/desafio2.js
+++ js/desafio2.js
@@ -1,12 +1,21 @@
 //guardo en la constante formulario todo el formulario con el id juego
 const formulario = document.getElementById('juego');
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const ingreso = document.getElementById('ingreso').value;
+    if ((ingreso === "coyote")) {
+        document.getElementById('ingreso').classList.add('xd');
+    } else {
+        document.getElementById('ingreso').classList.remove('xd');
+    }
+})
 const formulario2 = document.getElementById('juego');
 formulario.addEventListener('submit', function(e) {
     e.preventDefault();
     const ingreso = document.getElementById('ingreso2').value;
-    if ((ingreso2 != "COYOTE")) {
-        document.getElementById('enviar').classList.add('xd');
+    if ((ingreso === "coyote")) {
+        document.getElementById('ingreso2').classList.add('xd');
     } else {
-        document.getElementById('enviar').classList.remove('xd');
+        document.getElementById('ingreso2').classList.remove('xd');
     }
 })
