24f3d79de66435718d65d22e8a67629c97fefe9d
DLorangg <lorang.damian.epet20@gmail.com>
2022-11-14 16:51:20 -0300

    Desafios

--- js/desafio1.js
+++ js/desafio1.js
@@ -2,7 +2,6 @@ let estado = false;
 const logo = document.getElementById("logo");
 logo.addEventListener("click", moverPag);
 const mareo = document.getElementById("mareo");
-
 const frase = document.createElement("h1");
 frase.textContent = "AYUDAAAA  AAA AAAAAAAAAAAAAAAAAA ME MAREOoooo PARAMEEEEEEE si podes. . . jijiji";
 
@@ -11,13 +10,9 @@ function moverPag() {
         estado = true;
         document.getElementById('fondo').classList.add('animacion');
         mareo.appendChild(frase);
-
-
     } else {
         mareo.removeChild(frase);
-
         estado = false;
         document.getElementById('fondo').classList.remove('animacion');
-
     }
 }
--- js/desafio2.js
+++ js/desafio2.js
@@ -1,21 +1,12 @@
 //guardo en la constante formulario todo el formulario con el id juego
 const formulario = document.getElementById('juego');
-formulario.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const ingreso = document.getElementById('ingreso').value;
-    if ((ingreso === "coyote")) {
-        document.getElementById('ingreso').classList.add('xd');
-    } else {
-        document.getElementById('ingreso').classList.remove('xd');
-    }
-})
 const formulario2 = document.getElementById('juego');
 formulario.addEventListener('submit', function(e) {
     e.preventDefault();
     const ingreso = document.getElementById('ingreso2').value;
-    if ((ingreso === "coyote")) {
-        document.getElementById('ingreso2').classList.add('xd');
+    if ((ingreso2 != "COYOTE")) {
+        document.getElementById('enviar').classList.add('xd');
     } else {
-        document.getElementById('ingreso2').classList.remove('xd');
+        document.getElementById('enviar').classList.remove('xd');
     }
 })
